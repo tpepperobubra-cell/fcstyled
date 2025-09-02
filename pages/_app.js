f97f52b1b1d7d60f3b84ec6fb754e2742e2ead13
tpepperobubra-cell <tpepperobubra@gmail.com>
2025-09-02 08:27:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,5 @@
-import '../styles/globals.css'; import Header from '../components/Header'; import Footer from '../components/Footer'; export default function App({Component,pageProps}){ return (<><Header /><main><Component {...pageProps} /></main><Footer /></>) }
+import "@/styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
